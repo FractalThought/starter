a79bccb3c9e7f3ceb63a1f81c358505a1b2fede4
= <felix.thalin@live.com>
2019-10-01 11:51:20 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Stat from "./Components/Stat";
 
 function App() {
+  const character = {
+    name: "Fractale",
+    age: 29,
+    race: "Dragonborn",
+    traits: ["Nerdy", "Merciful", "Helpful"],
+    stats: [
+      { statsName: "Strength", statsValue: 5 },
+      { statsName: "Agility", statsValue: 5 },
+      { statsName: "Stamina", statsValue: 10 }
+    ]
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>DnD Character Creator</h1>
+      <p>
+        {character.name} {character.age}
+      </p>
+      <h2>Traits</h2>
+      <ul>
+        <li>{character.traits[0]}</li>
+        <li>{character.traits[1]}</li>
+        <li>{character.traits[2]}</li>
+      </ul>
+      <ul>
+        <Stat stat={character.stats[0]}></Stat>
+        <Stat stat={character.stats[1]}></Stat>
+        <Stat stat={character.stats[2]}></Stat>
+      </ul>
     </div>
   );
 }
