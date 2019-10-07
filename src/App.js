5ae1be11669593daca3ebbe361f154a07b0c958c
= <felix.thalin@live.com>
2019-10-07 09:41:10 +0200

    Added comments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Stat from "./Components/Stat";
+import Stat from "./Components/Stat"; // Importera komponenten Stat från Components-foldern
 
 function App() {
+  /*
+    Skapar ett objekt som håller i vår karaktär
+    Namn: "string"
+    Age: number
+    Race: "string"
+    Traits: []
+    Stats: [{StatsName, StatsValue}]
+  */
+
   const character = {
     name: "Fractale",
     age: 29,
@@ -16,6 +24,11 @@ function App() {
     ]
   };
 
+  /*
+    Return är det som skrivs ut på sidan
+    Det som returneras är JSX, och vi behöver returnera ett huvudelement
+    (i det här fallet <div className="App"></div>)
+  */
   return (
     <div className="App">
       <h1>DnD Character Creator</h1>
@@ -29,6 +42,10 @@ function App() {
         <li>{character.traits[2]}</li>
       </ul>
       <ul>
+        {/* (så här kommenterar vi i JSX) */}
+        {/*
+          Använder komponenten Stat och skickar med attributet (prop) stat
+        */}
         <Stat stat={character.stats[0]}></Stat>
         <Stat stat={character.stats[1]}></Stat>
         <Stat stat={character.stats[2]}></Stat>
@@ -37,4 +54,5 @@ function App() {
   );
 }
 
+// Export gör att funktionen går att komma åt från andra filer
 export default App;
--- src/Components/Stat.js
+++ src/Components/Stat.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+/*
+  Vår funktionella komponent Stat
+  Hämtar in stat som prop, och returnerar ett li-element
+*/
 function Stat({ stat }) {
   return (
     <li>
